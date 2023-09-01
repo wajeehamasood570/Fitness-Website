3a6fd4d9fa14b52759944c8be6a59295b70c808e
wajeehamasood570 <wajeehamasood570@gmail.com>
2023-09-02 00:59:51 +0500

    footer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Navbar } from 'react-bootstrap';
 import Navbars from './components/Navbars';
+import Footer from './components/Footer';
+import AboutGym from './components/AboutGym';
 
 function App() {
   return (
     <div>
       <Navbars/>
+      <AboutGym/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/AboutGym.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import about from '../assests/img/gallery2/about.png'
+import './AboutGym.css'
+
+const AboutGym = () => {
+    return (
+        <>
+            <Container >
+                <Row className='m-5'>
+                    <Col md={6} sm={12}>
+                        <img src={about} />
+                    </Col>
+                    <Col md={6} sm={12}>
+                        <div className='section-tittle'>
+                            <span>
+                                ABOUT oUR GYM
+                            </span>
+                            <h2>Safe Body building proper Solutions That Saves our Valuable Time!</h2>
+                            <p className="pera-top py-5">Brook presents your services with flexible, convenient and cdpose layouts. You can select your favorite layouts &amp; elements for cular ts with unlimited ustomization possibilities. Pixel-perfect replication of the designers is intended.</p>
+                            <p class="mb-65 pera-bottom">Brook presents your services with flexible, convefnient and chient anipurpose layouts. You can select your favorite layouts.</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default AboutGym
